294138a3c6d9fd90903ce0a9f65b813f87a68cb6
spadekarin <69378446+spadekarin@users.noreply.github.com>
2020-08-22 15:35:08 +0900

    Update mbit.js

--- mbit.js
+++ mbit.js
@@ -1,8 +1,10 @@
 var microBitBle;
 var adt7410;
 var readEnable;
-var microBitBle;
 var channel;
+
+var gpioPort0;
+var blinkEnable;
 
 async function connect() {
   // chirimen with micro:bitの初期化
@@ -10,10 +12,27 @@ async function connect() {
   msg.innerHTML = "micro:bitとのBLE接続が完了しました";
 
   // webSocketリレーの初期化
-  var relay = RelayServer("achex", "chirimenSocket");
-  channel = await relay.subscribe("chirimenMatrix");
+  //var relay = RelayServer("achex", "chirimenSocket");
+  //channel = await relay.subscribe("chirimenMatrix");
+  var relay = RelayServer(
+    "websocket.in",
+    "6kYyjth9cQTEvNEHuVDnh4pj3LQFK07DcwGi8BsgaAbKWf7qtCY5n8aoTr0h"
+  );
+  channel = await relay.subscribe("chirimenChuo");
   msg.innerText = "achex web socketリレーサービスに接続しました";
   channel.onmessage = printMessage;
+
+  var i2cAccess = await microBitBle.requestI2CAccess();
+  var i2cPort = i2cAccess.ports.get(1);
+  adt7410 = new ADT7410(i2cPort, 0x48);
+  await adt7410.init();
+  readEnable = true;
+
+  var gpioAccess = await microBitBle.requestGPIOAccess();
+  var mbGpioPorts = gpioAccess.ports;
+  gpioPort0 = mbGpioPorts.get(0);
+  await gpioPort0.export("out"); //port0 out
+  blinkEnable = true;
 
   // microbitが接続されたら関数を呼び出す
   pollButtonPush();
@@ -25,14 +44,35 @@ async function disconnect() {
   msg.innerHTML = "micro:bit BLE接続を切断しました。";
 }
 
+async function LEDblink() {
+  var gpio0Val = 0;
+  var count = 0;
+  while (blinkEnable) {
+    gpio0Val = gpio0Val === 1 ? 0 : 1; // 条件 (三項) 演算子
+    await gpioPort0.write(gpio0Val);
+    //msg.innerHTML = gpio0Val;
+    count++;
+    if (count == 10) {
+      break;
+    }
+    await sleep(1000);
+  }
+}
+
 //messageをmicrobitのLEDで表示する関数
 async function printMessage(message) {
   if (message.data.print) {
     var iconNumb = message.data.print;
-    msg.innerText = "get print icon command :" + iconNumb;
+    msg.innerText = "get print icon command :" + Math.floor(iconNumb);
     channel.send({ start: iconNumb }); // 表示の開始を知らせるメッセージを通知
-    await microBitBle.printLED(iconNumb); // micro:bitに表示を指示する(表示の完了に時間がかかる)
+    await microBitBle.printLED(Math.floor(iconNumb)); // micro:bitに表示を指示する(表示の完了に時間がかかる)
     channel.send({ done: iconNumb }); // 表示の完了を知らせるメッセージを通知
+    if (iconNumb > 28 && iconNumb < 40) {
+      await microBitBle.printLED("HOT");
+      LEDblink();
+    } else if (iconNumb < 20) {
+      await microBitBle.printLED("DARK");
+    }
   }
 }
 
@@ -48,10 +88,13 @@ async function pollButtonPush() {
       temTd.innerText = sensorData.temperature;
       briTd.innerText = sensorData.brightness;
       butTd.innerText = sensorData.button;
-      var temp = sensorData.temperature.toString();
+
+      var temp; // = sensorData.temperature.toString();
       var brt = sensorData.brightness.toString();
       if (sensorData.button == 1) {
-        channel.send({ print: temp });
+        temp = await adt7410.read();
+
+        channel.send({ print: String(Math.floor(temp)) });
       } else if (sensorData.button == 2) {
         channel.send({ print: brt });
       }
@@ -59,15 +102,13 @@ async function pollButtonPush() {
     await sleep(200);
   }
 }
-//値を読み通信する関数（未完）
+/*//値を読み通信する関数
 async function readData() {
   var readVal;
   while (readEnable) {
     readVal = await adt7410.read();
     console.log("readVal:", readVal);
     msg.innerHTML = "温度: " + readVal + "℃";
-    Value = String(readVal);
-    channel.send({ print: value });
     await sleep(1000);
   }
   //   while (readEnable) {
@@ -80,4 +121,4 @@ async function readData() {
   //   var data = pollButtonPush();
   //相手からrelayサーバを通して明るさを取得
   //得られた値を出力する
-}
+}*/
