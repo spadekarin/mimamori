917bda71c1fa268ff34989d2e84b4701d5085fe3
spadekarin <69378446+spadekarin@users.noreply.github.com>
2020-08-25 18:02:30 +0900

    Update mbit.js

--- mbit.js
+++ mbit.js
@@ -6,6 +6,9 @@ var channel;
 var gpioPort0;
 var blinkEnable;
 
+var npix;
+var neoPixels = 64; // LED個数
+
 async function connect() {
   // chirimen with micro:bitの初期化
   microBitBle = await microBitBleFactory.connect();
@@ -34,6 +37,9 @@ async function connect() {
   await gpioPort0.export("out"); //port0 out
   blinkEnable = true;
 
+  npix = new NEOPIXEL_I2C(i2cPort, 0x41);
+  await npix.init(neoPixels);
+  msg.innerHTML = "micro:bit neopixelを初期化しました。";
   // microbitが接続されたら関数を呼び出す
   pollButtonPush();
 }
@@ -42,6 +48,25 @@ async function disconnect() {
   readEnable = false;
   await microBitBle.disconnect();
   msg.innerHTML = "micro:bit BLE接続を切断しました。";
+}
+
+async function nPixTest1() {
+  console.log("nPix:", npix);
+  await npix.setGlobal(10, 0, 0);
+  await sleep(200);
+  await npix.setGlobal(0, 10, 0);
+  await sleep(200);
+  await npix.setGlobal(0, 0, 10);
+  await sleep(200);
+  await npix.setGlobal(0, 20, 20);
+  await sleep(200);
+  await npix.setGlobal(20, 0, 20);
+  await sleep(200);
+  await npix.setGlobal(20, 20, 0);
+  await sleep(200);
+  await npix.setGlobal(20, 20, 20);
+  await sleep(200);
+  await npix.setGlobal(0, 0, 0);
 }
 
 async function LEDblink() {
@@ -67,7 +92,7 @@ async function printMessage(message) {
     channel.send({ start: iconNumb }); // 表示の開始を知らせるメッセージを通知
     await microBitBle.printLED(String(Math.floor(iconNumb))); // micro:bitに表示を指示する(表示の完了に時間がかかる)
     channel.send({ done: iconNumb }); // 表示の完了を知らせるメッセージを通知
-    if (iconNumb > 28 && iconNumb < 40) {
+    if (iconNumb >= 28 && iconNumb <= 40) {
       await microBitBle.printLED("HOT");
       LEDblink();
     } else if (iconNumb < 20) {
@@ -102,6 +127,68 @@ async function pollButtonPush() {
     await sleep(200);
   }
 }
+
+async function setPattern0(iH) {
+  startH = 0;
+  if (iH) {
+    startH = iH;
+  }
+  var grbArray = [];
+  var nLED = npix.N_LEDS;
+  //	var nLED = 10;
+  for (var i = 0; i < nLED; i++) {
+    var h = startH + (360 * i) / nLED;
+    var s = 1;
+    var v = 0.05;
+    var rgb = hsvToRgb(h, s, v);
+    grbArray.push(rgb[1]);
+    grbArray.push(rgb[0]);
+    grbArray.push(rgb[2]);
+    //		await npix.setPixel(i , rgb[0],rgb[1],rgb[2]);
+  }
+  await npix.setPixels(grbArray);
+}
+
+// from https://qiita.com/hachisukansw/items/633d1bf6baf008e82847
+function hsvToRgb(H, S, V) {
+  //https://en.wikipedia.org/wiki/HSL_and_HSV#From_HSV
+
+  H = H % 360;
+
+  var C = V * S;
+  var Hp = H / 60;
+  var X = C * (1 - Math.abs((Hp % 2) - 1));
+
+  var R, G, B;
+  if (0 <= Hp && Hp < 1) {
+    [R, G, B] = [C, X, 0];
+  }
+  if (1 <= Hp && Hp < 2) {
+    [R, G, B] = [X, C, 0];
+  }
+  if (2 <= Hp && Hp < 3) {
+    [R, G, B] = [0, C, X];
+  }
+  if (3 <= Hp && Hp < 4) {
+    [R, G, B] = [0, X, C];
+  }
+  if (4 <= Hp && Hp < 5) {
+    [R, G, B] = [X, 0, C];
+  }
+  if (5 <= Hp && Hp < 6) {
+    [R, G, B] = [C, 0, X];
+  }
+
+  var m = V - C;
+  [R, G, B] = [R + m, G + m, B + m];
+
+  R = Math.floor(R * 255);
+  G = Math.floor(G * 255);
+  B = Math.floor(B * 255);
+
+  return [R, G, B];
+}
+
 /*//値を読み通信する関数
 async function readData() {
   var readVal;
@@ -122,3 +209,4 @@ async function readData() {
   //相手からrelayサーバを通して明るさを取得
   //得られた値を出力する
 }*/
+
